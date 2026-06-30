17e04660c665cb14f4571e166193490685ec4b52
Antoine <azanledjimawuko16@gmail.com>
2026-06-30 21:04:01 +0000

    Ajout de nouvelles fonctionnalités

--- script.js
+++ script.js
@@ -38,5 +38,22 @@ document.querySelectorAll('.nav ul li a').forEach(link => {
     overlay.classList.remove('show');
     menuToggle.classList.remove('active');
   });
+  // localisation//
+  const mapFrame = document.getElementById("mapFrame");
+  const openMapBtn = document.getElementById("openMapBtn");
+
+  // Affiche un message quand la carte est chargée
+  mapFrame.addEventListener("load", () => {
+    console.log("Carte Google Maps chargée !");
+    alert("La carte est prête !");
+  });
+
+  // Bouton pour ouvrir la carte dans un nouvel onglet
+  openMapBtn.addEventListener("click", () => {
+    window.open(
+      "https://www.google.com/maps/place/6.098765432109876,1.2345678901234567",
+      "_blank"
+    );
+  });
 });
   /**/
